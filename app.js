1cc669e7b07c0112ad29d5de20da160fdfabaa1b
SanjayTalya <sanjaytj@gmail.com>
2020-07-26 13:25:49 +0530

    accesing objects in json

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
-console.log('starting')
-setTimeout(() => {
-    console.log('2 second timeout')
-}, 2000)
+const request = require('request')
 
-setTimeout(() => {
-    console.log('second 0 timeout')
-}, 0)
-
-console.log('stopping')
+const url = "http://api.weatherstack.com/current?access_key=b0c04e0821fac880aa37a2b54f50c7eb&query=37.8267,-122.4233"
+request({  url: url, json: true }, (error, response) => {
+    console.log(response.body.current.temperature)
+    console.log('It is currently ' +response.body.current.temperature + ' degrees out. It feels it is 5 degrees out')
+})
