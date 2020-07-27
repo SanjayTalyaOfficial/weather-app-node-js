aaa6fbf0748a03a4bdf13f10d9cdbd075888ce82
SanjayTalya <sanjaytj@gmail.com>
2020-07-28 01:05:37 +0530

    callback chain - enter your own place for finding the weather

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
 // const request = require('request')
-
 const request = require("request")
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-geocode('Boston', (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-})
+const address = process.argv[2]
 
-forecast(-75.7088, 44.1545, (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-  })
+if (!address) {
+    console.log('Please provide an adress')
+} else {
+    geocode('Boston', (error, data) => {
+        if (error) {
+            return console.log(error)
+        } 
+        
+        forecast(data.latitude, data.longitude, (error, forecastData) => {
+            if (error) {
+                return console.log(error)
+            }
+    
+            console.log(data.location)
+            console.log(forecastData)
+        })
+    })
+    
+    
+
+}
+
+console.log(process.argv)
+
